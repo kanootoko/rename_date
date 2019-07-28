cb7d35dd28bdabf008d0d4df3a1298da211cd038
kanootoko <kanootoko@gmail.com>
2019-07-28 16:17:49 +0300

    Done: 	1. Added primary functional which has been missing, now script actually renames files. 	2. Added an editing pattern for output files command. 	3. Fixed creation time getting function, before it used some other file's time. 	4. Tested a bit more, fixed some small errors.
    Todo:
    	1. Maybe add some other features to output pattern, like numbers.
    	2. Maybe add bat-output feature.
    	3. Maybe add some sort of changes-on-run history, to rename files back if something goes wrong.

--- README.md
+++ README.md
@@ -14,3 +14,7 @@ You need to start the script in folder where your files are holded or choose the
     + ".*\.(jpg|avi)" - any file which ends with dot and "jpg" or "avi"
     + ".*\.[jJ][pP][gG]" - any file which ends with dot and "jpg" in whichever case
     + "IMG-[0-9]+.*" - any file which starts with "IMG-", then comes at least one digit, and then anything
++ --output_pattern=<time.strftime pattern> or -op=<time.strftime pattern> [default: Cam %Y-%m-%d %H_%S]
+  It's the pattern to rename files. Their format will not be changed,
+  and if there are some of them with the same patterned-names, "_#" with number will be added.
+  Number will have leading zeros if needed. Default pattern leads to "Cam YYYY-MM-DD HH_SS_(#).format"-like output names.
--- rename_date.py
+++ rename_date.py
@@ -1,8 +1,13 @@
-'''rename_date - this script allows to rename multiple files chosen by pattern (regular expression) to "Cam YYYY-MM-DD HH_SS_(#)"
+'''
+rename_date - this script allows to rename multiple files chosen by regular expression to user-defined pattern
 Usage: start script in folder where your files are holded or choose the directory by --input_folder command line argument.
+
 Possible command line arguments:
 	--input_folder=<path/to/folder> [default: . ]
-	--regular_expression=<regexp> [default: .*\\.([jJ][pP][gG]|[aA][vV][iI]|[tT][hH][mM]) ]
+	--regular_expression=<regexp> [default: .*\\.([jJ][pP][gG]|[aA][vV][iI]|[tT][hH][mM])]
+	--output_pattern=<time.strftime pattern> [default: Cam %Y-%m-%d %H_%M_%S ]
+
+Version: 1.1 [2019.07.28]
 '''
 
 import os, sys
@@ -10,12 +15,15 @@ from re import compile as re_compile, match as re_match, error as re_error
 from math import log, ceil
 
 class properties:
-	def __init__(self, path: str = os.curdir, regexp: str = '.*\\.([jJ][pP][gG]|[aA][vV][iI]|[tT][hH][mM])'):
+	def __init__(self, path: str = os.curdir, regexp: str = '.*\\.([jJ][pP][gG]|[aA][vV][iI]|[tT][hH][mM])', \
+	             pattern: str = 'Cam %Y-%m-%d %H_%M_%S', dry_run: bool = False):
 		self.path = path
 		try:
 			self.re = re_compile(regexp)
 		except re_error:
 			self.re = re_compile('.*\\.([jJ][pP][gG]|[aA][vV][iI]|[tT][hH][mM])')
+		self.pattern = pattern
+		self.dry_run = dry_run
 
 def parse_args(props: properties, argv: list):
 	for arg in argv[1:]:
@@ -30,20 +38,24 @@ def parse_args(props: properties, argv: list):
 				props.re = re_compile(arg[arg.find('=') + 1:])
 			except re_error:
 				print('Error setting regular expression: "{}"'.format(arg[arg.find('=') + 1:]))
+		elif arg.lower().startswith(('--output_pattern=', '-op=')):
+			props.pattern = arg[arg.find('=') + 1:]
 		elif arg.lower() in ('help', '/?', '--help', '-h', '-help'):
 			print(__doc__)
 			exit(0)
+		elif arg.lower() == '--dry_run':
+			props.dry_run = True
 		else:
 			print('Unknown parameter: "{}". Try "{} --help"'.format(arg, sys.argv[0]))
 
-def main(argv):
+def main(argv: list):
 	import time
 	props = properties()
 	if len(argv) > 1:
 		parse_args(props, argv)
 	filenames_map = {}
 	for filename in filter(lambda f: os.path.isfile(props.path + os.path.sep + f) and re_match(props.re, f) is not None, os.listdir(props.path)):
-		t = int(os.stat(props.path + os.sep + filename).st_mtime)
+		t = int(os.stat(props.path + os.sep + filename).st_ctime)
 		if t in filenames_map:
 			if isinstance(filenames_map[t], str):
 				filenames_map[t] = [filenames_map[t], filename]
@@ -53,13 +65,21 @@ def main(argv):
 			filenames_map.update({t: filename})
 	for t, filenames in filenames_map.items():
 		if isinstance(filenames, str):
-			new_name = time.strftime('Cam %Y-%m-%d %H_%S', time.strptime(time.ctime(t))) + filename[filename.rfind('.'):]
-			print('rename "{}" "{}"'.format(props.path + os.path.sep + filename, props.path + os.path.sep + new_name), file=sys.stderr)
+			new_name = time.strftime(props.pattern, time.strptime(time.ctime(t))) + filenames[filenames.rfind('.'):]
+			print('rename "{}" "{}"'.format(os.path.abspath(props.path + os.path.sep + filenames), \
+			      os.path.abspath(props.path + os.path.sep + new_name)), \
+			      file = sys.stdout if props.dry_run else sys.stderr)
+			if not props.dry_run:
+				os.rename(props.path + os.path.sep + filenames, props.path + os.path.sep + new_name)
 		else:
 			for i, filename in enumerate(filenames):
-				new_name = time.strftime('Cam %Y-%m-%d %H_%S', time.strptime(time.ctime(t)))
+				new_name = time.strftime(props.pattern, time.strptime(time.ctime(t)))
 				new_name += '_{0:0>{w}}{1}'.format(i + 1, filename[filename.rfind('.'):] if '.' in filename else '', w = ceil(log(len(filenames) + 1, 10)))
-				print('rename "{}" "{}"'.format(props.path + os.path.sep + filename, props.path + os.path.sep + new_name), file=sys.stderr)
+				print('rename "{}" "{}"'.format(os.path.abspath(props.path + os.path.sep + filename), \
+				      os.path.abspath(props.path + os.path.sep + new_name)), \
+				      file = sys.stdout if props.dry_run else sys.stderr)
+				if not props.dry_run:
+					os.rename(props.path + os.path.sep + filename, props.path + os.path.sep + new_name)
 		
 if __name__ == '__main__':
 	main(sys.argv)
